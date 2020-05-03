250f34d47df653104af74d1c31653693d0c6a48f
Tomek Lewandowski <tomasz.eln90@gmail.com>
2020-05-03 22:33:03 +0200

    Updates

---
+++ precache-manifest.67e68a6bcbac2041119a58770586c070.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b573d5c7fd9066c322a56f7b1dcef5",
+    "url": "/phone-store-page/index.html"
+  },
+  {
+    "revision": "3bf18122ca2bdb353a7d",
+    "url": "/phone-store-page/static/css/main.872eddb6.chunk.css"
+  },
+  {
+    "revision": "d250eab46c04ed087cab",
+    "url": "/phone-store-page/static/js/2.668892c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-store-page/static/js/2.668892c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf18122ca2bdb353a7d",
+    "url": "/phone-store-page/static/js/main.58cb9997.chunk.js"
+  },
+  {
+    "revision": "37d34dfc81e581a656f0",
+    "url": "/phone-store-page/static/js/runtime-main.27dec341.js"
+  },
+  {
+    "revision": "8c3f39e39863d96c9182b278adc80bc7",
+    "url": "/phone-store-page/static/media/a7.8c3f39e3.jpg"
+  },
+  {
+    "revision": "3072b587d3816661240e03cfa56d21d7",
+    "url": "/phone-store-page/static/media/htc.3072b587.jpg"
+  },
+  {
+    "revision": "1f3a69aca3273fd493c16387a8542cb8",
+    "url": "/phone-store-page/static/media/huawei.1f3a69ac.jpg"
+  },
+  {
+    "revision": "f96ba3ba14f973d13561d4050d642c35",
+    "url": "/phone-store-page/static/media/iphone.f96ba3ba.jpg"
+  },
+  {
+    "revision": "1a21d8ba3ed2f7bf99d37ff1b651fd9d",
+    "url": "/phone-store-page/static/media/nova 5t.1a21d8ba.jpg"
+  },
+  {
+    "revision": "d324be948c63d9bbb428682d8d28a9ec",
+    "url": "/phone-store-page/static/media/p30.d324be94.jpg"
+  },
+  {
+    "revision": "35de46b77b229a635303561302def229",
+    "url": "/phone-store-page/static/media/s10.35de46b7.jpg"
+  },
+  {
+    "revision": "40673d8ad8243fca8314cb0d7e8b934e",
+    "url": "/phone-store-page/static/media/s5.40673d8a.jpg"
+  },
+  {
+    "revision": "1f88097e1c2b2a01e1a7dabc5a78388d",
+    "url": "/phone-store-page/static/media/samsung.1f88097e.jpg"
+  },
+  {
+    "revision": "3d24f65a140ab428ae95d94f836b01eb",
+    "url": "/phone-store-page/static/media/xiaomi.3d24f65a.jpg"
+  },
+  {
+    "revision": "2c12ddcc8b4cb2cb124ce4bf7b15287b",
+    "url": "/phone-store-page/static/media/y7.2c12ddcc.jpg"
+  }
+]);
